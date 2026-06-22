50e4636a6deb0f044363a0e2e30632f62030259f
bruryansah <bruryansah@gmail.com>
2026-06-22 23:16:01 +0700

    Add filter functionality to array of objects in latihan12.js

--- JavaScript - L01/latihan12.js
+++ JavaScript - L01/latihan12.js
@@ -1,8 +1,14 @@
 
-//
+// Latihan 12: Filter Array of Objects
 
 const users = [
     { nama: "Ahmad", umur: 18 },
     { nama: "Budi", umur: 20 },
     { nama: "Siti", umur: 17 }
 ];
+
+const dewasa = users.filter(function(user) {
+    return user.umur >= 18;
+});
+
+console.log(dewasa);
